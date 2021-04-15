9f8f9135fe0adba18a458b078263d895c6437a41
Gabriel Gomes <gabrielgomes12394@gmail.com>
2021-04-15 16:56:10 -0300

    user_service

--- src/domain/business/users/User_controller.js
+++ src/domain/business/users/User_controller.js
@@ -1,11 +1,10 @@
-const {
-    findAllUserActive,
-    findUserByCpf,
-    updateUserByCpf,
-    safeDeleteUserByCpf
-} = require('./User_repository')
-
-const { serviceCreateUser } = require('./User_service')
+const { 
+    serviceCreateUser,
+    serviceGetAllActiveUsers,
+    serviceGetUserByCpf,
+    serviceSafeDeleteUser,
+    serviceUpdateUserByCpf
+ } = require('./User_service')
 
 const { JWT } = require('../../../infrastructure/config/jwt')
 const express = require('express')
@@ -13,77 +12,80 @@ const router = express.Router()
 
 router.post('/create',  async ( req, res )=>{
     try{
-
         const response = await serviceCreateUser(req.body.cpf, req.body)
-        res.status(201).send(response)
-
-    }catch(e){
-        res.status(500).send({
-            error:{
-                message:"Failed to create user"
+            if(response === 'User already exists'){
+                console.log("User already exists")
+                res.status(500).send({error: "User already exists"})
+            }else{
+                console.log(response)
+                res.status(201).send(response)
             }
-        })
+    }catch(e){
+        res.status(500).send({error:"Failed to create user"})
         console.error(e,"Failed to create user")
     }
 })
 
 router.get('/findallactive', JWT , async ( req, res )=>{
     try{
-        const activeUsers = await findAllUserActive()
-        res.status(200).send(activeUsers[0])
-        
-    }catch(e){
-        res.status(500).send({
-            error:{
-                message:"Users not Founds"
+        const response = await serviceGetAllActiveUsers()
+            if(response === "Error Users Not Found"){
+                console.log("Error Users Not Found")
+                res.status(500).send({error: "Error Users Not Found"})
+            }else{
+                console.log(response)
+                res.status(200).send(response)
             }
-        })
+    }catch(e){
+        res.status(500).send({error:"Error Users Not Found"})
         console.error(e,"Users not Founds\n")
     }
 })
 
 router.get('/findbycpf/:cpf', JWT , async ( req, res )=>{
     try{
-        const user = await findUserByCpf( req.params.cpf )
-        res.status(200).send(user)
-    }catch(e){
-        res.status(500).send({
-            error:{
-                message:"User not Found"
+        const response= await serviceGetUserByCpf( req.params.cpf )
+            if(response === "Error User Not Found"){
+                console.log("Error User Not Found")
+                res.status(500).send({error: "Error User Not Found"})
+            }else{
+                console.log(response)
+                res.status(200).send(response)
             }
-        })
+    }catch(e){
+        res.status(500).send({error:"User not Found"})
         console.error(e,"User not Found\n")
     }
 })
 
 router.delete('/deletebycpf/:cpf', JWT , async ( req, res )=>{
     try{
-        const userToDelete = await safeDeleteUserByCpf(req.params.cpf)
-        res.status(200).send({
-            deleted:"true"
-        })
-    }catch(e){
-        res.status(500).send({
-            error:{
-                message:"User not Found",
-                error:"Not deleted"
+        const response = await serviceSafeDeleteUser(req.params.cpf)
+            if(response === "Error User Not Found"){
+                console.log("Error User Not Found")
+                res.status(500).send({error:"Error User Not Found" })
+            }else{
+                console.log("Successfully Deleted")
+                res.status(200).send({success: "Successfully Deleted"})
             }
-        })
+    }catch(e){
+        res.status(500).send({error:"User not Found"})
         console.error(e,"User not Found\n")
     }
 })
 
 router.put('/updatebycpf/:cpf', JWT , async ( req, res )=>{
     try{
-        const userToUpdate = await updateUserByCpf(req.params.cpf)
-        res.status(200).send(userToUpdate)
-    }catch(e){
-        res.status(500).send({
-            error:{
-                message:"User not Found",
-                error:"Not updated"
+        const response = await serviceUpdateUserByCpf(req.params.cpf, req.body)
+            if(response === "Error User Not Found"){
+                console.log("Error User Not Found")
+                res.status(500).send({error:"Error User Not Found" })
+            }else{
+                console.log("Successfully Updated")
+                res.status(200).send(response)
             }
-        })
+    }catch(e){
+        res.status(500).send({error:"User not Found"})
         console.error(e,"User not Found\n")
     }
 })
